3174b117ac6607796eeb31d7cf85ed91ff2bc654
Minyeong Jeong <megormin@gmail.com>
2025-02-04 20:03:05 +0100

    wip car details

---
+++ src/components/CarDetails.tsx
@@ -0,0 +1,65 @@
+const CarDetails = () => {
+  return (
+    <div className=" flex w-[327px] flex-col gap-7 lg:flex-row font-display">
+      <figure className="card bg-base-100 shadow-sm rounded-box grid  grow place-items-center">
+        <img
+          src="https://res.cloudinary.com/dg1qeccqc/image/upload/v1712567777/Cars/Golf.webp	"
+          alt="Volkswagen Golf"
+        />
+      </figure>
+      <div className="card bg-base-100 shadow-sm rounded-box grid  grow gap-[30px] p-[30px]">
+        <div>
+          <h1 className="text-[28px] font-bold text-blue-950">
+            Volkswagen Golf
+          </h1>
+          <div className="flex items-center gap-2.5">
+            {" "}
+            <p className="text-lg text-amber-400">★★★☆☆</p>
+            <p className="text-sm text-neutral-500">2 Reviewer</p>
+          </div>{" "}
+        </div>
+        <div className="flex flex-col gap-3.5">
+          <ul className="text-sm text-neutral-400 flex flex-col gap-3.5">
+            <li className="flex justify-between">
+              <p>Type Car</p> <p className="text-neutral-600">Sedan</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Gear</p> <p className="text-neutral-600">Manuel</p>
+            </li>
+            <li className="flex justify-between">
+              <p>HP</p> <p className="text-neutral-600">110</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Color</p> <p className="text-neutral-600">Blue</p>
+            </li>
+          </ul>
+          <ul className="text-sm text-neutral-400 flex flex-col gap-3.5">
+            <li className="flex justify-between">
+              <p>Capacity</p> <p className="text-neutral-600">5 Persons</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Usage</p> <p className="text-neutral-600">7 L</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Fuel</p> <p className="text-neutral-600">Gasoline</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Laggage</p> <p className="text-neutral-600">2</p>
+            </li>
+          </ul>
+        </div>
+        <div className="card-actions flex-col items-start gap-5">
+          <p className="text-2xl font-bold ">
+            € 50 / <span className="text-neutral-400 text-base">day</span>
+          </p>
+          <button className="btn btn-primary w-full text-sm">Rent Now</button>
+        </div>
+      </div>
+      <div className="card bg-base-100 shadow-sm rounded-box grid  grow place-items-center">
+        content
+      </div>
+    </div>
+  );
+};
+
+export default CarDetails;
