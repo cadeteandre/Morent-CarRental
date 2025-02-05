7d8836464c3da390e0b930205347d1c8d18d12bf
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-02-05 11:56:08 +0100

    edit: interface Vehicle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import AdCard from "../components/AdCard";
-import { Tables } from "../utils/supabase/supabase";
 import { supabase } from "../utils/supabase/setupSupabase";
 import AutoCard from "../components/autoCard";
 
-type Vehicle = Tables<"vehicles">;
+type Vehicle = {
+    brand: { name: string };
+    consumption: number;
+    gear_type: "Automatic" | "Manuel";
+    model: string;
+    price_per_day: number | null;
+    seats: number;
+    vehicle_type: { name: string };
+    car_img: string | null;
+};
 
 const Home = () => {
     const [fetchedVehicle, setFetchedVehicle] = useState<Vehicle[]>([]);
 
     useEffect(() => {
         async function fetchVehicles() {
-            const { data, error } = await supabase.from("vehicles").select();
+            const { data, error } = await supabase.from("vehicles").select("brand(*), consumption, gear_type, model, price_per_day, seats, vehicle_type(*), car_img ");
             if (error) {
                 console.error("Fehler beim Fetchen der Vehicles: ", error);
             } else {
@@ -33,7 +41,7 @@ const Home = () => {
             </section>
             <section>
                 {fetchedVehicle.map((vehicle, i) => (
-                    <AutoCard key={i} brand={vehicle.brand} consumption={vehicle.consumption} gear_type={vehicle.gear_type} model={vehicle.model} price_per_day={vehicle.price_per_day} seats={vehicle.seats} vehicle_type={vehicle.vehicle_type} car_img={vehicle.car_img} />
+                    <AutoCard key={i} brand={vehicle.brand.name} consumption={vehicle.consumption} gear_type={vehicle.gear_type} model={vehicle.model} price_per_day={vehicle.price_per_day} seats={vehicle.seats} vehicle_type={vehicle.vehicle_type.name} car_img={vehicle.car_img} />
                 ))}
             </section>
         </>
