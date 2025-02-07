e7a115a97d5686e1a190f03216dcc05f2edeb666
Minyeong Jeong <megormin@gmail.com>
2025-02-07 10:46:20 +0100

    login done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,16 @@
 import { useRef, useState } from "react";
 import { supabase } from "../utils/supabase/setupSupabase";
 import { useUserContext } from "../UserContext";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export const Login = () => {
   const emailRef = useRef<HTMLInputElement>(null!);
   const passwordRef = useRef<HTMLInputElement>(null!);
 
-  //   const [message, setMessage] = useState<string | null>(null);
-  //   const [isError, setIsError] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
   const { setUser } = useUserContext();
+  const navigate = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -20,64 +20,67 @@ export const Login = () => {
       password: passwordRef.current.value,
     });
 
-    console.dir(result);
-
     if (result.data.user) {
       setUser(result.data.user);
-
-      //   setMessage("Du bist eingeloggt.");
-      //   setIsError(false);
+      navigate("/");
     } else {
       console.error(result.error);
-      //   setMessage("Login fehlgeschlagen.");
-      //   setIsError(true);
+      setIsError(true);
     }
   };
 
   return (
-    <section className="bg-white p-6">
-      <div className="bg-slate-100 rounded-xl shadow-lg overflow-hidden w-72 mx-auto p-8">
-        <h3 className="text-xl font-semibold pb-2 text-center">Login</h3>
-        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-          <div className="flex flex-col w-full">
-            <label htmlFor="email" className="label">Email:</label>
+    <section className="pt-8 pb-16">
+      <fieldset className="fieldset  shadow-lg  rounded-lg  mx-auto p-8 flex items-center justify-center flex-col w-sm mb-32 font-Jakarta-Regular">
+        <h3 className="text-2xl font-Jakarta-Regular font-semibold pb-8 text-center">
+          Login
+        </h3>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+          <div className="w-full flex flex-col gap-0.5">
+            <label className="text-sm" htmlFor="emailInput">
+              Email
+            </label>
             <input
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-              type="text"
-              name="Email"
+              className="w-full input"
+              type="email"
+              id="emailInput"
               ref={emailRef}
+              placeholder="email@morent.com"
             />
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="password" className="label">Password:</label>
+          <div className="w-full flex flex-col gap-0.5">
+            <label className="text-sm" htmlFor="password">
+              Password
+            </label>
             <input
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full input"
               type="password"
-              name="password"
+              id="password"
               ref={passwordRef}
+              placeholder="********"
             />
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <button className="btn bg-blue-600 text-white w-full mb-6">
-              Sign in
-            </button>
-            <p className="text-black">
-              No account? Register{" "}
-              <Link className="text-blue-500" to={"/register"}>
-                Here
-              </Link>
-            </p>
-            <button className="btn bg-white w-full">
-              Forgot Password?
-            </button>
-          </div>
-        </form>
-        {/* {message && (
-          <p className={`${isError ? "text-red-800" : "text-blue-700"}`}>
-            {message}
+          <button
+            type="submit"
+            className="btn btn-primary py-3.5 text-base font-Jakarta-SemiBold "
+          >
+            Sign in
+          </button>{" "}
+          {isError ? <p>Login failed.</p> : ""}
+          <p className="text-base mt-10 text-center">
+            No account? Register{" "}
+            <Link className="text-info" to={"/register"}>
+              Here
+            </Link>
           </p>
-        )} */}
-      </div>
+          <button
+            type="button"
+            className="btn btn-outline py-3.5 text-xs font-Jakarta-SemiBold w-full"
+          >
+            Forgot Password?
+          </button>
+        </form>
+      </fieldset>
     </section>
   );
 };
