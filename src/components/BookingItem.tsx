358f93c7af7fb8f0dd63e154946802e980aa5c68
Minyeong Jeong <megormin@gmail.com>
2025-02-10 14:40:06 +0100

    bookings page done

--- src/components/BookingItem.tsx
+++ src/components/BookingItem.tsx
@@ -29,43 +29,46 @@ const BookingItem: React.FC<IBookingItemProps> = ({
   mapNumber,
 }) => {
   return (
-    <div className="flex flex-col w-full max-w-[1440px] mb-8">
-      <p className="font-bold text-left ml-6 mb-2">{pickupDate}</p>
-      <div className="flex flex-col rounded-lg py-4 bg-white">
-        <div className="flex flex-col rounded-xl px-6">
-          <div className="flex gap-2 items-center sm:self-start">
+    <div className="flex flex-col w-full  mb-8">
+      <p className="font-bold text-left text-lg ml-6 mb-2">{pickupDate}</p>
+      <div className="flex flex-col rounded-lg p-7 bg-white">
+        <div className="flex gap-7 ">
+          <Link
+            to={`/details/${carId}`}
+            className="  rounded-lg flex items-center justify-center  w-[100px] h-[80px] overflow-hidden "
+          >
+            <img
+              src={carImg}
+              alt="Booking Car"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </Link>
+          <div className="flex flex-col gap-1.5  md:gap-2.5 items-start  font-Jakarta-SemiBold">
             <Link to={`/details/${carId}`}>
-              <img
-                src={carImg}
-                alt="Booking Car"
-                className="max-w-[100px] rounded-xl"
-              />
+              <p className="font-bold font-Jakarta-SemiBold">
+                {carBrand} {carModel}
+              </p>
             </Link>
-            <div className="text-left">
-              <Link to={`/details/${carId}`}>
-                <p className="font-bold">
-                  {carBrand} {carModel}
-                </p>
-              </Link>
-              <p className="font-extralight text-xs">
-                {pickupDate} - {dropOffDate}
-              </p>
-              <p className="font-bold">
-                {calculateTotalPrice(price, pickupDate, dropOffDate)} €
-              </p>
+            <div className="text-xs text-neutral-500 flex flex-col items-start md:flex-row">
+              <p>{pickupDate} - </p>
+              <p>{dropOffDate}</p>
+            </div>
+
+            <p className="font-bold">
+              {calculateTotalPrice(price, pickupDate, dropOffDate)} €
+            </p>
+          </div>
+        </div>
+        <hr className="w-full self-center my-4 opacity-50" />
+        <div className="flex items-center gap-10">
+          <div className="flex gap-4 px-2">
+            <img src="/svg/pick-drop-icon.svg" alt="Pickup Drop-off Icon" />
+            <div className="flex flex-col items-start justify-between">
+              <p className="text-xs text-neutral-500">{pickupCity}</p>
+              <p className="text-xs text-neutral-500">{dropOffCity}</p>
             </div>
           </div>
-          <hr className="w-full self-center my-4 opacity-50" />
-          <div className="flex items-center gap-10">
-            <div className="flex gap-4 px-2">
-              <img src="/svg/pick-drop-icon.svg" alt="Pickup Drop-off Icon" />
-              <div className="flex flex-col justify-between">
-                <p className="text-xs">{pickupCity}</p>
-                <p className="text-xs">{dropOffCity}</p>
-              </div>
-            </div>
-            <LeafletMap pickupCity={pickupCity} mapNumber={mapNumber} />
-          </div>
+          <LeafletMap pickupCity={pickupCity} mapNumber={mapNumber} />
         </div>
       </div>
     </div>
